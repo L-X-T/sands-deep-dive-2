0a8e452ad83268c92b4c95bcc949b5b17331e18f
Alex Thalhammer <alex@angular-software.at>
2022-01-23 18:20:50 +0100

    03_d: Using HostBinding and HostListener

--- src/app/shared/controls/click-with-warning.directive.ts
+++ src/app/shared/controls/click-with-warning.directive.ts
@@ -1,12 +1,28 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostBinding, HostListener, Input, OnInit, Output } from '@angular/core';
 
 @Directive({
   selector: '[appClickWithWarning]'
 })
 export class ClickWithWarningDirective implements OnInit {
+  // Input und Output ergänzen:
+  @Input() warning = 'Are you sure?';
+  @Output() appClickWithWarning = new EventEmitter();
+
+  // HostBinding ergänzen
+  @HostBinding('class') classBinding: string | undefined;
+
   constructor(private elementRef: ElementRef) {}
 
+  // HostListener ergänzen:
+  @HostListener('click', ['$event.shiftKey'])
+  handleClick(shiftKey: boolean): void {
+    if (shiftKey || confirm(this.warning)) {
+      this.appClickWithWarning.emit();
+    }
+  }
+
   ngOnInit(): void {
-    this.elementRef.nativeElement.setAttribute('class', 'btn btn-danger');
+    // Klassen über HostBinding zuweisen:
+    this.classBinding = 'btn btn-danger';
   }
 }
