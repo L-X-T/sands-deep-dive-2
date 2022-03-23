529eae1d1b7859e8d7e4deb38dd9986644781695
Alex Thalhammer <alex@angular-software.at>
2022-03-23 10:03:22 +0100

    06b_tdf: Bonus: Asynchronous Multifield Validator

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -21,6 +21,7 @@ import { CanDeactivateGuard } from './deactivation/can-deactivate.guard';
 import { CityValidatorDirective } from './validation/city-validator.directive';
 import { AsyncCityValidatorDirective } from './validation/async-city-validator.directive';
 import { MultiFieldValidatorDirective } from './validation/multi-field-validator.directive';
+import { AsyncMultiFieldValidatorDirective } from './validation/async-multi-field-validator.directive';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
@@ -40,7 +41,8 @@ import { MultiFieldValidatorDirective } from './validation/multi-field-validator
 
     CityValidatorDirective,
     AsyncCityValidatorDirective,
-    MultiFieldValidatorDirective
+    MultiFieldValidatorDirective,
+    AsyncMultiFieldValidatorDirective
   ],
   exports: [
     DateComponent,
@@ -60,7 +62,8 @@ import { MultiFieldValidatorDirective } from './validation/multi-field-validator
 
     CityValidatorDirective,
     AsyncCityValidatorDirective,
-    MultiFieldValidatorDirective
+    MultiFieldValidatorDirective,
+    AsyncMultiFieldValidatorDirective
   ]
 })
 export class SharedModule {
