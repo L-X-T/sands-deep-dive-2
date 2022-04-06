751638776cf43ff5c525adc6258a260cbe7eb687
Alex Thalhammer <alex@angular-software.at>
2022-04-06 16:00:22 +0200

    09b: Bonus: Implement an UI Library

--- nx-workspace/libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ nx-workspace/libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
 import { RouterModule } from '@angular/router';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
 
 @NgModule({
   imports: [
     CommonModule,
     LuggageDomainModule,
+
+    // Add this:
+    LuggageUiCardModule,
 
     // Add this:
     RouterModule.forChild([{ path: '', component: CheckinComponent }])
