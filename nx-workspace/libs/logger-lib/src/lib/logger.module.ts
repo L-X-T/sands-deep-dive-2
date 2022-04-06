3ff651a234d4588f55ab69d336ed69a0a03fced1
Alex Thalhammer <alex@angular-software.at>
2022-04-06 14:08:55 +0200

    09a: Bonus: Adapt your library

--- nx-workspace/libs/logger-lib/src/lib/logger.module.ts
+++ nx-workspace/libs/logger-lib/src/lib/logger.module.ts
@@ -1,25 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LogMonitorComponent } from './log-monitor.component';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { DefaultLogFormatterService } from './default-log-formatter.service';
+import { LogFormatterService } from './log-formatter.service';
 import { LoggerConfig } from './logger.config';
 
-// imports: [ LoggerModule.forRoot({ ... }) ]
+const defaultFormatterConfig = [
+  {
+    provide: LogFormatterService,
+    useClass: DefaultLogFormatterService
+  }
+];
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [LogMonitorComponent],
-  // providers: [
-  //   LoggerService
-  // ],
-  exports: [LogMonitorComponent]
+  declarations: []
 })
 export class LoggerModule {
-  // Setup
   static forRoot(config: LoggerConfig): ModuleWithProviders<LoggerModule> {
     return {
       ngModule: LoggerModule,
-      providers: [{ provide: LoggerConfig, useValue: config }]
+      providers: [
+        { provide: LoggerConfig, useValue: config },
+        !config.logFormatterType ? defaultFormatterConfig : { provide: LogFormatterService, useClass: config.logFormatterType }
+      ]
     };
   }
 }
