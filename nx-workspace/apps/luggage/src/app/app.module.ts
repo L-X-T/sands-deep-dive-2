08e3d6034062cfd5df1f459143b1cab06bfb2da4
Alex Thalhammer <alex@angular-software.at>
2022-04-06 16:25:49 +0200

    09b: Add Preloading

--- nx-workspace/apps/luggage/src/app/app.module.ts
+++ nx-workspace/apps/luggage/src/app/app.module.ts
@@ -5,32 +5,35 @@ import { AppComponent } from './app.component';
 import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
 import { HttpClientModule } from '@angular/common/http';
 import { LuggageFeatureReportLossModule } from '@flight-workspace/luggage/feature-report-loss';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    LuggageFeatureCheckinModule,
     HttpClientModule,
-    LuggageFeatureReportLossModule,
+    // LuggageFeatureCheckinModule,
+    // LuggageFeatureReportLossModule,
 
     // Add these routes:
-    RouterModule.forRoot([
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'check-in'
-      },
-      {
-        path: 'check-in',
-        loadChildren: () => import('@flight-workspace/luggage/feature-checkin').then((m) => m.LuggageFeatureCheckinModule)
-      },
-      {
-        path: 'report-loss',
-        loadChildren: () => import('@flight-workspace/luggage/feature-report-loss').then((m) => m.LuggageFeatureReportLossModule)
-      }
-    ])
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'check-in'
+        },
+        {
+          path: 'check-in',
+          loadChildren: () => import('@flight-workspace/luggage/feature-checkin').then((m) => m.LuggageFeatureCheckinModule)
+        },
+        {
+          path: 'report-loss',
+          loadChildren: () => import('@flight-workspace/luggage/feature-report-loss').then((m) => m.LuggageFeatureReportLossModule)
+        }
+      ],
+      { preloadingStrategy: PreloadAllModules }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
